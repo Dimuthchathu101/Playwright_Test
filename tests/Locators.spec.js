d86737cd15f68351271adab51e02403e439a1a58
Dimuthchathu101 <dimuthchathu101@gmail.com>
2023-12-31 08:34:38 +0530

    Locators

---
+++ tests/Locators.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+test('First Test Case', async ({page}) => { 
+    await page.goto('https://demoblaze.com/index.html');
+
+    await page.click('id=login2')
+
+    // await page.locator("id=login2").fill("Hello World");
+
+    await page.fill('id=login2', 'dimuthcbandara')
+
+    // checking the password
+    await page.fill('id=loginpassword','password')
+
+    const logoutLink = await page.locator("//a[normalize-space()='About us']")
+
+    await expect(logoutLink).toBeVisible();
+    
+    page.close()
+ })
